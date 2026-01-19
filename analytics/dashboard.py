1c4aab8e2270bf2385239b6f5ccd8116925fc07e
Siarhei Paulavets <s.paulavets@gmail.com>
2026-01-20 02:09:47 +0300

    Add dev manual reloading to dashboard (caching problem)

--- analytics/dashboard.py
+++ analytics/dashboard.py
@@ -1,3 +1,5 @@
+import os
+
 import streamlit as st
 
 from widgets.divider import write_divider
@@ -6,6 +8,11 @@ from widgets.sales_section import write_sales_section
 from widgets.products_section import write_product_section
 
 if __name__ == '__main__':
+    if os.getenv("env") == "dev":
+        if st.button('🔄 Reload'):
+            st.cache_data.clear()
+            st.rerun()
+
     conn = st.connection('data_warehouse', type='sql')
 
     st.set_page_config(page_title='Analytics Dashboard')
